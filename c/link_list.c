1665c54573791a200609f7cc30624e25e0b40c25
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-18 23:29:44 +0530

    Update link_list.c

--- c/link_list.c
+++ c/link_list.c
@@ -1,10 +1,11 @@
-/* linked list */
 #include<stdio.h>
+
 struct node
 {
   int data;
   struct node* link;
 };
+
 struct node *START=NULL;
 struct node* createnode()
 {
@@ -12,9 +13,10 @@ struct node* createnode()
   n=(struct node*)malloc(sizeof(struct node));
   return n;
 }
+
 void insertNode()
 {
-  struct node *temp,*t;
+  struct node *temp, *t;
   temp=createnode();
   printf("Enter a value: ");
   scanf("%d",&temp->data);
@@ -22,7 +24,7 @@ void insertNode()
   if(START==NULL)
   {
     START=temp;
-    printf("Node is added at address=%d value=%d",temp,temp->data);
+    printf("Node is created at Address: %d and its value is: %d",temp,temp->data);
   }
   else
   {
@@ -32,48 +34,53 @@ void insertNode()
       t=t->link;
     }
     t->link=temp;
-    printf("Node is added at address=%d value=%d",temp,temp->data);
+    printf("Node is created at Address: %d and its value is: %d",temp,temp->data);
   }
 }
+
 void deleteNode()
 {
   struct node *r;
   if(START==NULL)
   {
-    printf("List is Empty");
+    printf("Linked-List is empty!");
   }
   else
   {
     r=START;
-    START=START->link;
-    printf("Node is Deleted at address=%d value=%d",r,*r);
-    free(r);
+    while(r!=NULL)
+    {
+      printf("Node Address is: %d and its value is",r,*r);
+      free(r);
+    }
   }
 }
+
 void viewNode()
 {
   struct node *t;
   if(START==NULL)
   {
-    printf("List is Empty");
+    printf("Linked-List is empty!");
   }
   else
   {
     t=START;
     while(t!=NULL)
     {
-      printf("Node value=%d address=%d\n",t->data,t);
+      printf("Node Address is: %d and its value is: ",t,t->data);
       t=t->link;
     }
   }
 }
+
 int length()
 {
   struct node *t;
   int len=0;
-  if (START==NULL)
+  if(START==NULL)
   {
-    printf("List is Empty");
+    printf("Linked-List is empty!");
     return 0;
   }
   else
@@ -84,24 +91,25 @@ int length()
       t=t->link;
       len=len++;
     }
-  return len;
+    return len;
   }
 }
+
 void deleteLocation()
 {
-  struct node *t,*p,*q;
+  struct node *t, *p, *q;
   int loc,i=1;
-  printf("Enter the location: ");
+  printf("Enter location you want to delete node at: ");
   scanf("%d",&loc);
-  if (loc>length())
+  if(loc>length())
   {
-    printf("Invalid location: ");
+    printf("Invalid Location");
   }
   else if(loc==1)
   {
-    t=START;
-    START=t->link;
-    t->link=NULL;
+  t=START;
+  START=t->link;
+  t->link=NULL;
   }
   else
   {
@@ -111,46 +119,40 @@ void deleteLocation()
       p=p->link;
       i++;
     }
-  q=p->link;
-  p->link=q->link;
-  q->link=NULL;
-  free(q);
+    q=p->link;
+    p->link=q->link;
+    q->link=NULL;
+    free(q);
   }
 }
 
 void main()
 {
-  int ch;
+  int choice;
   while(1)
   {
     clrscr();
-    printf("\n1. Insert a node");
-    printf("\n2. Delete a node");
-    printf("\n3. View the list of data present in list");
-    printf("\n4. Delete at particular location");
-    printf("\n5. Exit");
-    printf("\nEnter a choice: ");
-    scanf("%d",&ch);
-    switch(ch)
+    printf("///Linked-List Program///\n");
+    printf("\n 1: Insert a Node");
+    printf("\n 2: Delete a Node");
+    printf("\n 3: View every Node");
+    printf("\n 4: Delete Node at a particular location");
+    printf("\n 5: Exit");
+    printf("\n\nEnter a choice: ");
+    scanf("%d",&choice);
+    switch(choice)
     {
-      case 1:
-	    insertNode();
-	    break;
-      case 2:
-	    deleteNode();
-	    break;
-      case 3:
-	    viewNode();
-	    break;
-      case 4:
-	    deleteLocation();
-	    break;
-
-      case 5:
-	    exit(0);
-	    break;
-      default:
-	     printf("Invalid Entry");
+      case 1: insertNode();
+              break;
+      case 2: deleteNode();
+              break;
+      case 3: viewNode();
+              break;
+      case 4: deleteLocation();
+              break;
+      case 5: exit(0);
+              break;
+      default: printf("Invalid Choice entered.");
     }
     getch();
   }
