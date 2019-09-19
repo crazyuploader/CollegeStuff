daf5f6b3cf2b082e358f88b4fc8ba6ec55ffe81b
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-19 11:25:34 +0530

    Create multiplication.cpp

---
+++ cpp/multiplication.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+#define NEWLINE '\n'
+#define TAB '\t
+
+int main()
+{
+  int a,b;
+  cout<< "///Program to display multiplication table of entered number///"<<endl<<endl<<endl;
+  cout<< "Enter 1st number: ";
+  cin>> a;
+  cout<< "Enter 2nd number: ";
+  cin>> b;
+  cout<< "Answer is "<< a*b;
+  return 0;
+}
