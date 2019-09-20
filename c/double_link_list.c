157090708a7d33b047db4601d30f92094411ebed
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-20 13:35:46 +0530

    Update double_link_list.c

--- c/double_link_list.c
+++ c/double_link_list.c
@@ -1,8 +1,40 @@
 #include<stdio.h>
 
-struct node;
+struct node
 {
   int data;
-  struct node* left;
-  struct node* right;
+  struct node* left; //Pointer to left node
+  struct node* right; //Pointer to right node
 };
+
+struct node *START=NULL;
+
+void append()
+{
+    struct node *temp;
+    temp=(struct node*)malloc(sizeof(struct node));
+    printf("Please enter a value for a node: ");
+    scanf("%d",&temp->data);
+    temp->left=NULL;
+    temp->right=NULL;
+    if(START==NULL)
+    {
+        START=temp;
+    }
+    else
+    {
+        struct node* p;
+        p=START;
+        while(p->right!=NULL)
+        {
+            p=p->right;
+        }
+        p->right=temp;
+        temp->left=p;
+    }
+}
+
+int main()
+{
+    append();
+}
