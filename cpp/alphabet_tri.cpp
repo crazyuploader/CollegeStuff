76043b8f6b87eaf2599c1f9021e0fc7599c67c76
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-22 17:04:38 +0530

    Update alphabet_tri.cpp

--- cpp/alphabet_tri.cpp
+++ cpp/alphabet_tri.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
-
+    char ch='A';
+    int i,j,k,m;
+    cout<< "///Program to display alphabet triangle///"<< endl<< endl<< endl;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+            cout<< " ";
+        for(k=1;k<=i;k++)
+            cout<<ch++;
+            ch--;
+        for(m=1;m<i;m++)
+            cout<< --ch;
+        cout<< "\n";
+        ch='A';
+    }
+    cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+    return 0;
 }
-
