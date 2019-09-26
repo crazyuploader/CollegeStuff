58687ef61ee72ef7f4ce9d6b80bafe52b6d56c7c
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-26 13:14:54 +0530

    Update number_swap.cpp

--- cpp/number_swap.cpp
+++ cpp/number_swap.cpp
@@ -6,19 +6,30 @@ using namespace std;
 
 int main()
 {
-    int choice,a,b;
+    int choice,a,b,c;
     cout<< "///Program to Swapping two entered numbers///"<< endl<< endl<< endl;
     while(1)
     {
         cout<< "\nchoices\n";
-        cout<< "\n1. for Swapping using + and -";
-        cout<< "\n2. for Swapping using * and /";
+        cout<< "\n1. for Swapping using third variable";
+        cout<< "\n2. for Swapping using + and -";
+        cout<< "\n3. for Swapping using * and /";
         cout<< "\nAnything else and off you go!\n";
         cout<< "\nchoice: ";
         cin>> choice;
         switch(choice)
         {
-            case 1:  cout<< "\nUsing + and -"<< endl;
+            case 1:  cout<< "\nUsing third variable";
+                     cout<< "\nEnter number a: ";
+                     cin>> a;
+                     cout<< "\nEnter number b: ";
+                     cin>> b;
+                     c=a;
+                     a=b;
+                     b=c;
+                     cout<< "After Swapping number a is "<< a<< " and number b is "<<b;
+                     break;
+            case 2:  cout<< "\nUsing + and -"<< endl;
                      cout<< "\nEnter number a: ";
                      cin>> a;
                      cout<< "\nEnter number b: ";
@@ -28,7 +39,7 @@ int main()
                      a=a-b;
                      cout<< "After swapping a is "<< a<< " and b is "<< b;
                      break;
-            case 2:  cout<< "\nUsing * and /"<< endl;
+            case 3:  cout<< "\nUsing * and /"<< endl;
                      cout<< "\nEnter number a: ";
                      cin>> a;
                      cout<< "\nEnter number b: ";
