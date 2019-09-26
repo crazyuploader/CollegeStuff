547e368121310e056be5ec77f211a16bd355273e
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-26 13:18:38 +0530

    Update main.cpp
    updated swap program

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -291,19 +291,30 @@ int sum_digits()
 
 int number_swap()
 {
-    int choice,a,b;
+    int choice,a,b,c;
     cout<< "///Program to Swapping two entered numbers///"<< endl<< endl<< endl;
     while(1)
     {
         cout<< "\nchoices\n";
-        cout<< "\n1. for Swapping using + and -";
-        cout<< "\n2. for Swapping using * and /";
+        cout<< "\n1. for Swapping using third variable";
+        cout<< "\n2. for Swapping using + and -";
+        cout<< "\n3. for Swapping using * and /";
         cout<< "\nAnything else and off you go!\n";
         cout<< "\nchoice: ";
         cin>> choice;
         switch(choice)
         {
-            case 1:  cout<< "\nUsing + and -"<< endl;
+            case 1:  cout<< "\nUsing third variable";
+                     cout<< "\nEnter number a: ";
+                     cin>> a;
+                     cout<< "\nEnter number b: ";
+                     cin>> b;
+                     c=a;
+                     a=b;
+                     b=c;
+                     cout<< "After Swapping a is "<< a<< " and b is "<<b;
+                     break;
+            case 2:  cout<< "\nUsing + and -"<< endl;
                      cout<< "\nEnter number a: ";
                      cin>> a;
                      cout<< "\nEnter number b: ";
@@ -311,9 +322,9 @@ int number_swap()
                      a=a+b;
                      b=a-b;
                      a=a-b;
-                     cout<< "After swapping a is "<< a<< " and b is "<< b;
+                     cout<< "After Swapping a is "<< a<< " and b is "<< b;
                      break;
-            case 2:  cout<< "\nUsing * and /"<< endl;
+            case 3:  cout<< "\nUsing * and /"<< endl;
                      cout<< "\nEnter number a: ";
                      cin>> a;
                      cout<< "\nEnter number b: ";
@@ -321,7 +332,7 @@ int number_swap()
                      a=a*b;
                      b=a/b;
                      a=a/b;
-                     cout<< "After swapping a is "<< a<< " and b is "<< b;
+                     cout<< "After Swapping a is "<< a<< " and b is "<< b;
                      break;
             default: cout<< "Exiting...\n\n";
                      cout<< TAB<< " =================================\n";
