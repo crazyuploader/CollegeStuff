103662a659d00190e4b6cebe900c65b6367d795a
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-26 14:45:50 +0530

    Update main.cpp
    adding Binary to Decimal Number conversion in main program

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -344,23 +344,73 @@ int number_swap()
     return 0;
 }
 
-int deci_to_bin()
+int convertDecimalToBinary()
 {
-    int i,num,r[10],j;
-    cout<< "///Program to convert entered Decimal number to Binary number///"<< endl<< endl<< endl;
-    cout<< "Enter number: ";
+    int i,num,deci_num,r[10],j;
+    cout<< "\nConverting Decimal Number to Binary Number"<< endl<< endl;
+    cout<< "\nEnter Decimal Number: ";
     cin>> num;
+    deci_num=num;
     for(i=0;num>0;i++)
     {
         r[i]=num%2;
         num=num/2;
     }
-    cout<< "\nBinary number of entered number is: ";
+    cout<< "Entered Decimal Number is = "<< deci_num<< endl;
+    cout<< "Its Binary equivalent is = ";
     for(j=i-1;j>=0;j--)
         {
             cout<< r[j];
         }
     return 0;
+}
+
+int convertBinaryToDecimal()
+{
+    long long num,bin_num;
+    int i,r=0,deci=0,base=1;
+    cout<< "\nConverting Binary Number to Decimal Number"<< endl<< endl;
+    cout<< "\nEnter binary number(1s and 0s): ";
+    cin>> num;
+    bin_num=num;
+    for(i=0;num>0;i++)
+    {
+        r=num%10;
+        deci=deci+r*base;
+        num=num/10;
+        base=base*2;
+    }
+    cout << "Entered Binary Number is = "<< bin_num<< endl;
+    cout << "Its Decimal Equivalent is = "<< deci;
+    return 0;
+}
+
+int number_conversion()
+{
+    long long n;
+    int choice;
+    cout<< "///Program to convert Numbers///"<< endl<< endl<< endl;
+    while(1)
+    {
+        cout<< "\nchoices\n";
+        cout<< "\n1. for Decimal to Binary";
+        cout<< "\n2. for Binary to Decimal";
+        cout<< "\nAnything else and off you go!\n";
+        cout<< "\nchoice: ";
+        cin>> choice;
+        switch(choice)
+        {
+            case 1:  convertDecimalToBinary();
+                     break;
+            case 2:  convertBinaryToDecimal();
+                     break;
+            default: cout<< "Exiting...\n\n";
+                     cout<< TAB<< " =================================\n";
+                     cout<< TAB<< "||Created by Jugal Kishore -- 2019||"<< NEWLINE;
+                     cout<< TAB<< " =================================\n";
+                     exit(0);
+        }
+    }
 }
 
 int sizeof_data_types()
@@ -520,7 +570,7 @@ int main()
               break;
      case 17: number_swap();
               break;
-     case 18: deci_to_bin();
+     case 18: number_conversion();
               break;
      case 19: sizeof_data_types();
               break;
