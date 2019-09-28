fb279f732c8f4dd52f290602236993906f8321d9
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-28 18:08:01 +0530

    Update string.cpp
    completed the full program

--- programs/cpp/string.cpp
+++ programs/cpp/string.cpp
@@ -16,22 +16,53 @@ int main()
     cout<< "\nEnter Last Name: ";
     cin>> l_name;
     cout<< "\nDo you have Landline or a Mobile Phone? ";
-    cout<< "\nPlease type 1 for Landline and 2 for Mobile Phone: ";
+    cout<< "\n\nPlease type 1 for Landline and 2 for Mobile Phone: ";
     cin>> temp;
     if(temp==1)
         {
             cout<< "\nEnter your Landline Number with STD code: ";
             cin>> phone;
-            if(phone.length()==10)
+            if(phone.length()==11)
                 {
-                    cout<< "\nEntered Landline Number is = "<< phone;
+                    cout<< "\nEntered Landline Number is OK";
                 }
             else
                 {
-                    cout<< "Check number and try again ";
+                    cout<< "Check Number and Try Again!";
+                    cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
                     exit(0);
                 }
         }
+    else
+        if(temp==2)
+            {
+                cout<< "\nEnter your Mobile Phone Number: ";
+                cin>> phone;
+                if(phone.length()==10)
+                    {
+                        cout<< "\nEntered Mobile Phone Number is OK";
+                    }
+                else
+                    {
+                        cout<< "Check Number and Try Again!";
+                        cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+                        exit(0);
+                    }
+            }
+        else
+        {
+            cout<< "\nYou haven't pressed 1 or 2, have you?\n\nExiting!";
+            cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
+            exit(0);
+        }
+    cout<< "\nEnter your Address: ";
+    cin>> address;
+    cout<< "\nEnter your Email Address: ";
+    cin>> email;
+    cout<< NEWLINE<< "Your Entered Details are as follows -";
+    cout<< "\nFull Name: "<< f_name<< " "<< l_name<< endl;
+    cout<< "\nPhone Number: "<< phone<< endl;
+    cout<< "\nEmail Address: "<< email;
     cout<< NEWLINE<< NEWLINE<< "Created by Jugal Kishore -- 2019"<< NEWLINE;
     return 0;
 }
