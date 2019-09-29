43918bec1dd843a47c9f6a97441cc707206ed25c
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-29 21:23:32 +0530

    Update main.cpp
    fixed some_info.cpp

--- programs/cpp/main.cpp
+++ programs/cpp/main.cpp
@@ -557,15 +557,15 @@ int some_info()
             cout<< TAB<< " =================================\n";
             exit(0);
         }
-    /* Not working at the moment ;)
     cout<< "\nEnter your Address: ";
-    cin>> address;
-    */
+    cin.ignore();
+    getline(cin,address);
     cout<< "\nEnter your Email Address: ";
     cin>> email;
-    cout<< NEWLINE<< "Your Entered Details are as follows -";
+    cout<< NEWLINE<< "Your Entered Details are as follows -"<< endl;
     cout<< "\nFull Name: "<< f_name<< " "<< l_name<< endl;
     cout<< "\nPhone Number: "<< phone<< endl;
+    cout<< "\nAddress: "<< address<< endl;
     cout<< "\nEmail Address: "<< email;
     return 0;
 }
