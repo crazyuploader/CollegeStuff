827f6a551605580d6f3c9edd462d36671b2209df
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-09-30 10:09:48 +0530

    Update main.cpp

--- programs/cpp/main.cpp
+++ programs/cpp/main.cpp
@@ -575,51 +575,38 @@ int calc()
     int a,b,result;
     char option;
     cout<< "///A Simple Calculator///"<< endl<< endl<< endl;
-    cout<< "\nEnter First Number: ";
-    cin>> a;
-    cout<< "\nEnter Second Number: ";
-    cin>> b;
-    cout<< "Options:\n";
-    cout<< "+ for Addition\n";
-    cout<< "- for Subtraction\n";
-    cout<< "* for Multiplication\n";
-    cout<< "/ for Division\n";
-    cout<< "choice: ";
-    cin>> option;
-    if(option=='+')
+    while(1)
+    {
+        cout<< "\nEnter First Number: ";
+        cin>> a;
+        cout<< "\nEnter Second Number: ";
+        cin>> b;
+        cout<< "\nOptions:\n";
+        cout<< "+ for Addition\n";
+        cout<< "- for Subtraction\n";
+        cout<< "* for Multiplication\n";
+        cout<< "/ for Division\n";
+        cout<< "And anything else to exit!\n";
+        cout<< "choice: ";
+        cin>> option;
+        switch(option)
         {
-            cout<< "\nAddition of "<< a<< " and "<< b<< " is "<< a+b;
-        }
-    else
-        {
-            if(option=='-')
-                {
-                    cout<< "\nDifference of "<< a<< " and "<< b<< " is "<< a-b;
-                }
-            else
-                {
-                    if(option=='*')
-                        {
-                            cout<< "\nMultiplication of "<< a<< " and "<< b<< " is "<< a*b;
-                        }
-                    else
-                        {
-                            if(option=='/')
-                                {
-                                    cout<< "\nDivision of "<< a<< " and "<< b<< " is "<< a/b;
-                                }
-                            else
-                                {
-                                    cout<< "\nUh-huh! You haven't entered the right option, have you?\n\nExiting!";
-                                    cout<< TAB<< " =================================\n";
-                                    cout<< TAB<< "||Created by Jugal Kishore -- 2019||"<< NEWLINE;
-                                    cout<< TAB<< " =================================\n";
-                                    exit(0);
-                                }
-                        }
+            case '+': cout<< "\nAddition of "<< a<< " and "<< b<< " is "<< a+b<< endl;
+                      break;
+            case '-': cout<< "\nDifference of "<< a<< " and "<< b<< " is "<< a-b<< endl;
+                      break;
+            case '*': cout<< "\nMultiplication of "<< a<< " and "<< b<< " is "<< a*b<< endl;
+                      break;
+            case '/': cout<< "\nDivision of "<< a<< " and "<< b<< " is "<< a/b<< endl;
+                      break;
+            default:  cout<< "\nUh-huh! You haven't entered the right option, have you?\n\nExiting!"<< endl<< endl;
+                      cout<< TAB<< " =================================\n";
+                      cout<< TAB<< "||Created by Jugal Kishore -- 2019||"<< NEWLINE;
+                      cout<< TAB<< " =================================\n";
+                      exit(0);
 
-                }
-        }
+            }
+    }
     return 0;
 }
 
