6f8e3d9341061e803fb6c08142cb8217ef543fda
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-07 14:24:30 +0530

    Update link_list.c

--- programs/c/link_list.c
+++ programs/c/link_list.c
@@ -1,159 +1,159 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct node
 {
-  int data;
-  struct node* link;
+    int data;
+    struct node* link;
 };
 
 struct node *START=NULL;
 struct node* createnode()
 {
-  struct node *n;
-  n=(struct node*)malloc(sizeof(struct node));
-  return n;
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    return n;
 }
 
 void insertNode()
 {
-  struct node *temp, *t;
-  temp=createnode();
-  printf("Enter a value: ");
-  scanf("%d",&temp->data);
-  temp->link=NULL;
-  if(START==NULL)
-  {
-    START=temp;
-    printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
-  }
-  else
-  {
-    t=START;
-    while(t->link!=NULL)
+    struct node *temp, *t;
+    temp=createnode();
+    printf("Enter a value: ");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(START==NULL)
     {
-      t=t->link;
+        START=temp;
+        printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
     }
-    t->link=temp;
-    printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
-  }
+    else
+    {
+        t=START;
+        while(t->link!=NULL)
+        {
+        t=t->link;
+        }
+        t->link=temp;
+        printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
+    }
 }
 
 void deleteNode()
 {
-  struct node *r;
-  if(START==NULL)
-  {
-    printf("Linked-List is empty!");
-  }
-  else
-  {
-    r=START;
-    while(r!=NULL)
+    struct node *r;
+    if(START==NULL)
     {
-      printf("Node Address is = %d and its value is =",r,*r);
-      free(r);
+        printf("Linked-List is empty!");
     }
-  }
+    else
+    {
+        r=START;
+        while(r!=NULL)
+        {
+            printf("Node Address is = %d and its value is =",r,*r);
+            free(r);
+        }
+    }
 }
 
 void viewNode()
 {
-  struct node *t;
-  if(START==NULL)
-  {
-    printf("Linked-List is empty!");
-  }
-  else
-  {
-    t=START;
-    while(t!=NULL)
+    struct node *t;
+    if(START==NULL)
     {
-      printf("Node Address is = %d and its value is = ",t,t->data);
-      t=t->link;
+        printf("Linked-List is empty!");
     }
-  }
+    else
+    {
+        t=START;
+        while(t!=NULL)
+        {
+        printf("Node Address is = %d and its value is = ",t,t->data);
+        t=t->link;
+        }
+    }
 }
 
 int length()
 {
-  struct node *t;
-  int len=0;
-  if(START==NULL)
-  {
-    printf("Linked-List is empty!");
-    return 0;
-  }
-  else
-  {
-    t=START;
-    while(t!=NULL)
+    struct node *t;
+    int len=0;
+    if(START==NULL)
     {
-      t=t->link;
-      len=len++;
+        printf("Linked-List is empty!");
+        return 0;
     }
-    return len;
-  }
+    else
+    {
+        t=START;
+        while(t!=NULL)
+        {
+        t=t->link;
+        len=len++;
+        }
+        return len;
+    }
 }
 
 void deleteLocation()
 {
-  struct node *t, *p, *q;
-  int loc,i=1;
-  printf("Enter location you want to delete node at: ");
-  scanf("%d",&loc);
-  if(loc>length())
-  {
-    printf("Invalid Location");
-  }
-  else if(loc==1)
-  {
-  t=START;
-  START=t->link;
-  t->link=NULL;
-  }
-  else
-  {
-    p=START;
-    while(i<loc-1)
+    struct node *t, *p, *q;
+    int loc,i=1;
+    printf("Enter location you want to delete node at: ");
+    scanf("%d",&loc);
+    if(loc>length())
     {
-      p=p->link;
-      i++;
+        printf("Invalid Location");
     }
-    q=p->link;
-    p->link=q->link;
-    q->link=NULL;
-    free(q);
-  }
+    else
+        if(loc==1)
+        {
+            t=START;
+            START=t->link;
+            t->link=NULL;
+        }
+        else
+        {
+            p=START;
+            while(i<loc-1)
+            {
+                p=p->link;
+                i++;
+            }
+            q=p->link;
+            p->link=q->link;
+            q->link=NULL;
+            free(q);
+        }
 }
 
 void main()
 {
-  int choice;
-  while(1)
-  {
-    clrscr();
-    printf("///Linked-List Program///\n");
-    printf("\n 1: Insert a Node");
-    printf("\n 2: Delete a Node");
-    printf("\n 3: View every Node");
-    printf("\n 4: Delete Node at a particular location");
-    printf("\n 5: Exit");
-    printf("\n\nEnter a choice: ");
-    scanf("%d",&choice);
-    switch(choice)
+    int choice;
+    while(1)
     {
-      case 1: insertNode();
-              break;
-      case 2: deleteNode();
-              break;
-      case 3: viewNode();
-              break;
-      case 4: deleteLocation();
-              break;
-      case 5: exit(0);
-              break;
-      default: printf("Invalid Choice entered.");
+        printf("///Linked-List Program///\n");
+        printf("\n 1: Insert a Node");
+        printf("\n 2: Delete a Node");
+        printf("\n 3: View every Node");
+        printf("\n 4: Delete Node at a particular location");
+        printf("\n 5: Exit");
+        printf("\n\nEnter a choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:  insertNode();
+                     break;
+            case 2:  deleteNode();
+                     break;
+            case 3:  viewNode();
+                     break;
+            case 4:  deleteLocation();
+                     break;
+            case 5:  exit(0);
+                     break;
+            default: printf("Invalid Choice entered.");
+        }
     }
-    getch();
-  }
 }
