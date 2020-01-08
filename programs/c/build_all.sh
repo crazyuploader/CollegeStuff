84dfdd6cee9132121531a446b049804580afa7a0
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2020-01-08 11:25:38 +0530

    Update build_all.sh

--- programs/c/build_all.sh
+++ programs/c/build_all.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 echo ""
 echo "Compiling Programs with GCC"
+echo ""
 for f in $(ls *.c); do echo "Compiling $f"; gcc $f; done
 echo ""
 echo ""
 echo "Compiling Programs with Clang"
+echo ""
 for f in $(ls *.c); do echo "Compiling $f"; clang $f; done
 echo ""
